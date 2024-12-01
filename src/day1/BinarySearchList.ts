3baae14fcb7df6deeaf0bef67e612dee77f39965
Stsiapan Zayalenchyts <stza.by@gmail.com>
2024-12-01 23:03:22 +0100

    Binary Search

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,22 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let result = false;
+
+    let l = 0;
+    let h = haystack.length;
+
+    do {
+        const m = Math.floor((l + h) / 2);
+        const v = haystack[m];
+
+        if (v === needle) {
+            result = true;
+            break;
+        } else if (v > needle) {
+            h = m;
+        } else {
+            l = m + 1;
+        }
+    } while (l < h);
+
+    return result;
+}
