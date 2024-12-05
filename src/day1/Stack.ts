c385b23e73068477a69b220f5651a9e3d1d758ad
Stsiapan Zayalenchyts <stza.by@gmail.com>
2024-12-05 23:28:33 +0100

    Stack implementation

---
+++ src/day1/Stack.ts
@@ -0,0 +1,44 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
+export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>;
+
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+    }
+
+    push(item: T): void {
+        this.length++;
+        const node = { value: item } as Node<T>;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+
+        const value = this.head.value;
+        this.head = this.head.prev;
+
+        return value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
