2169500680815f0ba268760647b966fd3373755f
Stsiapan Zayalenchyts <stza.by@gmail.com>
2025-01-12 11:26:48 +0100

    Compare binary trees

---
+++ src/day1/CompareBinaryTrees.ts
@@ -0,0 +1,12 @@
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (a === null && b == null) return true;
+
+    if (a === null || b === null) return false;
+
+    if (a.value !== b.value) return true;
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
+}
