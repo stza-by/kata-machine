45ca9985d6f64efd63c43d6467e4e3ec95d6a86a
Stsiapan Zayalenchyts <stza.by@gmail.com>
2025-01-12 10:27:01 +0100

    BT traversal not recursive

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,9 +1,9 @@
+import Stack from "@code/Stack";
+
 type Path<T> = T[];
 
 const walk = <T>(node: BinaryNode<T> | null, path: Path<T>) => {
-    if (!node) {
-        return path;
-    }
+    if (!node) return path;
 
     path.push(node.value);
 
@@ -13,6 +13,27 @@ const walk = <T>(node: BinaryNode<T> | null, path: Path<T>) => {
     return path;
 };
 
+const walkNotRecursive = <T>(node: BinaryNode<T>) => {
+    const path: Path<T> = [];
+    const stack = new Stack<BinaryNode<T>>();
+    stack.push(node);
+
+    while (stack.length) {
+        const node = stack.pop();
+
+        if (!node) continue;
+
+        path.push(node.value);
+
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+
+    return path;
+};
+
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-    return walk(head, []);
+    const path = walkNotRecursive(head);
+
+    return path;
 }
